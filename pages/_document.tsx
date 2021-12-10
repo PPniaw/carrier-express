13fee595aeefd3d67eec8169fa62e25cb88f44d7
ymcheung <ymcheung.tw@me.com>
2021-12-10 10:21:37 +0800

    Add splitbee

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+          <script data-respect-dnt async src="https://cdn.splitbee.io/sb.js" />
         </Head>
         <body>
           <Main />
