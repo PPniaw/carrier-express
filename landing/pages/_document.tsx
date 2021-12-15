6be139e73ec311cda60c6adc2cbac93805ee00d8
ymcheung <ymcheung.tw@me.com>
2021-12-15 20:06:14 +0800

    Add logo

--- landing/pages/_document.tsx
+++ landing/pages/_document.tsx
@@ -8,6 +8,9 @@ export default class Document extends NextDocument {
         <Head>
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
           <script data-respect-dnt async src="https://cdn.splitbee.io/sb.js" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
--- landing/pages/index.tsx
+++ landing/pages/index.tsx
@@ -1,19 +1,53 @@
+import { useEffect } from 'react';
 import { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 
+import Image from 'next/image';
+
+import { styled, globalCss } from 'stitches.config';
+
 import HeadMeta from '@utils/HeadMeta';
 import { Container } from '@components/layout';
 
+const pageBody = globalCss({
+  'body[data-body-style=home]': {
+    paddingTop: '$12',
+    backgroundColor: 'hsl($shade120)'
+  }
+});
+
+const SloganMain = styled('h1', {
+  margin: '0 0 $8',
+  color: 'hsl($shade10)',
+  fontFamily: '$default',
+  fontSize: '3.2rem',
+  lineHeight: '40px'
+});
+
+const SloganAffix = styled('span', {
+  color: 'hsl($shade30)',
+  fontFamily: '$default',
+  fontSize: '2rem'
+});
+
 export default function Home() {
   const { t } = useTranslation('landing');
+
+
+  useEffect(() => {
+    document.body.setAttribute('data-body-style', 'home');
+  });
+
+  pageBody();
 
   return(
     <>
       <HeadMeta />
       <Container responsive={{ '@m768': 'max640', '@m1200': 'max1168' }}>
-        Carrier Express
-        {t('link.home')}
+        <Image src="/logo.svg" width={144} height={40} alt="外出籠規畫" />
+        <SloganMain dangerouslySetInnerHTML={{__html: t('slogan.main')}} />
+        <SloganAffix dangerouslySetInnerHTML={{__html: t('slogan.affix')}} />
       </Container>
     </>
   )
