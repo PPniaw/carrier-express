0d56c0402881d6435d2e42281ceb320744ea6217
ymcheung <ymcheung.tw@me.com>
2021-12-17 17:48:42 +0800

    Update favicons

--- landing/components/Header.tsx
+++ landing/components/Header.tsx
@@ -1,13 +1,22 @@
+import Link from 'next/link';
 import Image from 'next/image';
 
 import { styled } from 'stitches.config';
 
 import { Container } from '@components/layout';
 
+const LogoLink = styled('a', {
+  display: 'inline-block'
+});
+
 export default function Header() {
   return(
     <Container as="header" header={{ '@initial': 'mobile', '@m992': 'desktop' }}>
-      <Image src="/logo.svg" width={144} height={40} alt="外出籠規畫" />
+      <Link href="/" passHref>
+        <LogoLink>
+          <Image src="/logo.svg" width={144} height={40} alt="外出籠規畫" />
+        </LogoLink>
+      </Link>
     </Container>
   )
 }
